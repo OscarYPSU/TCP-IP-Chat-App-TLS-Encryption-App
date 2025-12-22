61fb02ee16260c49924ff7921cae5db8d64ffcc7
root <root@Oscar.localdomain>
2025-12-21 22:39:39 -0500

    initial commit

---
+++ Client.c
@@ -0,0 +1,62 @@
+//
+// Created by oskhy on 12/21/2025.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE];
+
+    // creates the socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket creation error");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 address from human readable string to machine readable bytes
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("Invalid address / Address not supported\n");
+        return -1;
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Connection Failed\n");
+        return -1;
+    }
+
+    printf("Connect to server at\n");
+
+    while (1) {
+        // Send message to server
+        printf("You: ");
+        fgets(buffer, BUFFER_SIZE, stdin); //gets input from user keyboard
+        buffer[strcspn(buffer, "\n")] = '\0'; // remove newline
+        send(sock, buffer, strlen(buffer), 0);
+
+        // Receive message from server
+        memset(buffer, 0, BUFFER_SIZE);
+        int valread = read(sock, buffer, BUFFER_SIZE);
+        if (valread <= 0) {
+            printf("Server disconnected.\n");
+            break;
+        }
+        printf("Server: %s\n", buffer);
+    }
+
+    close(sock);
+    return 0;
+}
---
+++ Server.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, client_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE];
+
+
+    // creates the socket for server
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("SOCKET FAILED:SERVER");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // bind server port to address to listen or client port
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("CONNECTING SOCKET TO ADDRESS FAILED");
+        exit(EXIT_FAILURE);
+    }
+
+    // starts listening for any user client
+    if (listen(server_fd, 3) < 0) { // n = number of clients can queue to connect to server
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("SERVER STARTED LISTENING\n ON PORT: ", PORT);
+
+    // Accept client
+    if ((client_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("Accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Client connected!\n");
+
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+
+        // once client sends message
+        int valread = read(client_socket, buffer, BUFFER_SIZE);
+        if (valread <= 0) {
+            printf("Client disconnected.\n");
+            break;
+        }
+        printf("Client: %s\n", buffer);
+
+        // Send message to client
+        printf("You: ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        buffer[strcspn(buffer, "\n")] = '\0'; // remove newline
+        send(client_socket, buffer, strlen(buffer), 0);
+
+    }
+
+    close(client_socket);
+    close(server_fd);
+    return 0;
+}
